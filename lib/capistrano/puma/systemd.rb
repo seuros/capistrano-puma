58329e9afba0b25d889813ce4190fdc73900393f
Ben Dilley <ben@skillstream.co.uk>
2020-12-07 13:05:46 +0000

    Default systemd service name on multi-app host
    Using a default systemd service name that is consistent with the default
    names used in the nginx and monit plugins.
    
    In combination with seuros/capistrano-puma#308, forms a zero-config
    solution to seuros/capistrano-puma#305

--- lib/capistrano/puma/systemd.rb
+++ lib/capistrano/puma/systemd.rb
@@ -13,7 +13,7 @@ module Capistrano
     def set_defaults
       set_if_empty :puma_systemd_conf_dir, '/etc/systemd/system'
       set_if_empty :puma_systemctl_bin, '/bin/systemctl'
-      set_if_empty :puma_service_unit_name, 'puma'
+      set_if_empty :puma_service_unit_name, -> { "puma_#{fetch(:application)}_#{fetch(:stage)}" }
     end
   end
 end
