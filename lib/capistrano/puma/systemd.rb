9fd07a2bd972ac2c567bf4a0593b587ade527233
Chris Coetzee <chriscz93@gmail.com>
2021-04-04 20:59:11 +0200

    Add puma socket file and variables

--- lib/capistrano/puma/systemd.rb
+++ lib/capistrano/puma/systemd.rb
@@ -13,6 +13,7 @@ module Capistrano
     def set_defaults
       set_if_empty :puma_systemctl_bin, '/bin/systemctl'
       set_if_empty :puma_service_unit_name, -> { "puma_#{fetch(:application)}_#{fetch(:stage)}" }
+      set_if_empty :puma_enable_socket_service, -> { false }
       set_if_empty :puma_systemctl_user, :system
       set_if_empty :puma_enable_lingering, -> { fetch(:puma_systemctl_user) != :system }
       set_if_empty :puma_lingering_user, -> { fetch(:user) }
