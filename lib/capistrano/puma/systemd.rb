2677eac0ce382ab97efacf57bc8a12729f19c8e0
Abdelkader Boudih <terminale@gmail.com>
2022-09-22 00:18:47 +0100

    sync gems

--- lib/capistrano/puma/systemd.rb
+++ lib/capistrano/puma/systemd.rb
@@ -12,11 +12,11 @@ module Capistrano
 
     def set_defaults
       set_if_empty :puma_systemctl_bin, -> { fetch(:systemctl_bin, '/bin/systemctl') }
-      set_if_empty :puma_service_unit_name, -> { "puma_#{fetch(:application)}_#{fetch(:stage)}" }
-      set_if_empty :puma_enable_socket_service, -> { false }
-      set_if_empty :puma_phased_restart, -> { false }
+      set_if_empty :puma_service_unit_name, "puma_#{fetch(:application)}_#{fetch(:stage)}"
+      set_if_empty :puma_enable_socket_service, false
+      set_if_empty :puma_service_unit_env_file, fetch(:service_unit_env_file)
 
-      set_if_empty :puma_systemctl_user, -> { fetch(:systemctl_user, :user) }
+      set_if_empty :puma_systemctl_user, fetch(:systemctl_user, :user)
       set_if_empty :puma_enable_lingering, -> { fetch(:puma_systemctl_user) != :system }
       set_if_empty :puma_lingering_user, -> { fetch(:lingering_user, fetch(:user)) }
       set_if_empty :puma_access_log, -> { File.join(shared_path, 'log', "#{fetch(:puma_env)}.log") }
